b335c175018ff25393adcb1c965de136483a3c27
PC <PC@Gustavo>
2022-05-29 21:08:36 -0300

    aaaaaaaa

--- TPE-Grupo13/src/main/java/fi/unju/edu/ar/util/ListaCandidato.java
+++ TPE-Grupo13/src/main/java/fi/unju/edu/ar/util/ListaCandidato.java
@@ -12,10 +12,10 @@ public class ListaCandidato {
 	public ListaCandidato(){
         candidato = new ArrayList<Candidato>();
 
-        candidato.add(new Candidato(1010,"Freddy Duki", "Rock", "Canta Autor", 0));
-        candidato.add(new Candidato(1010,"Freddy Mercury", "Rock", "Canta Autor", 0));
-        candidato.add(new Candidato(1010,"Freddy Mercury", "Rock", "Canta Autor", 0));
-        candidato.add(new Candidato(1010,"Freddy Mercury", "Rock", "Canta Autor", 0));
+        candidato.add(new Candidato(1010,"Duki", "Trap", "Canta Autor", 0));
+        candidato.add(new Candidato(1011,"Nicky Nicole", "Trap", "Canta Autor", 0));
+        candidato.add(new Candidato(1012,"Paulo Londra", "Trap", "Canta Autor", 0));
+        candidato.add(new Candidato(1013,"Lit Killah", "Trap", "Canta Autor", 0));
     }
 
 	public ListaCandidato(ArrayList<Candidato> candidato) {
